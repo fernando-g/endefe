6023fb422dc400c29dd03ccdf834dceed0bc9743
dunzaga <dunzaga@12464a0d-5266-c320-e22b-786707fc7f0c>
2012-01-01 17:33:58 +0000

    nuevo dto

---
+++ src/FacturaElectronica.Common/Contracts/ComprobanteArchivoAsociadoDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacturaElectronica.Common.Contracts
+{
+    public class ComprobanteArchivoAsociadoDto
+    {
+        public long Id { get; set; }
+
+        public DateTime? FechaVencimiento { get; set; }
+
+        public int TipoComprobanteId { get; set; }
+
+        public string TipoComprobanteDescripcion { get; set; }
+
+        public long NroComprobante { get; set; }
+
+        public DateTime FechaDeCarga { get; set; }
+
+        public long? ClienteId { get; set; }
+
+        public string ClienteNombre { get; set; }
+
+        public int EstadoId { get; set; }
+
+        public string EstadoDescripcion { get; set; }
+
+        public string DireccionIp { get; set; }
+
+        public DateTime? FechaVisualizacion { get; set; }
+
+        public string CAE { get; set; }
+
+        public DateTime? CAEFechaVencimiento { get; set; }
+
+        public string PathArchivo { get; set; }
+
+        public DateTime? CbteFecha { get; set; }
+
+        public int? PtoVta { get; set; }
+
+        public int TipoContratoId { get; set; }
+
+        public string TipoContratoDescripcion { get; set; }
+    }
+}
