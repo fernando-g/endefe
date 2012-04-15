fbeb19a2f7dc771e9f4a7e954dd19dd7cda5b9a7
dunzaga <dunzaga@12464a0d-5266-c320-e22b-786707fc7f0c>
2012-04-15 02:14:10 +0000

    Log Criteria

---
+++ src/FacturaElectronica.Common/Contracts/LogCriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacturaElectronica.Common.Contracts
+{
+    [Serializable]
+    public class LogCriteria
+    {
+        public long CorridaId { get; set; }
+
+        public long? LogCorridaId { get; set; }
+    }
+}
